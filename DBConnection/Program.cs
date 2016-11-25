1d78933e30c2c033ed4a291f6115ca44307d28aa
philyphish <philipknigh@gmail.com>
2016-11-25 10:28:18 -0500

    Initial post

--- DBConnection/Program.cs
+++ DBConnection/Program.cs
@@ -6,6 +6,81 @@ using System.Threading.Tasks;
 
 namespace DBConnection
 {
+    //    They have a connection string
+    //• They can be opened
+    //• They can be closed
+    //• They may have a timeout attribute(so if the connection could not be opened within the
+    //timeout, an exception will be thrown).
+    //Your job is to represent these commonalities in a base class called DbConnection.This class
+    //should have two properties:
+    //ConnectionString : string
+    //Timeout : TimeSpan
+
+    public abstract class DBConnect
+    {
+        public string ConnectionString { get; set; }
+        public TimeSpan Timeout { get; set; }
+
+        public DBConnect()
+        {
+
+        }
+
+        public DBConnect(string connectionString)
+        {
+            if (String.IsNullOrWhiteSpace(connectionString))
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                connectionString = ConnectionString;
+                var comand = new DbCommand(connectionString);
+               
+            }
+            
+        }
+
+        public abstract void Open();
+        public abstract void Closed();
+        
+
+    }
+
+    public class SQLConnection : DBConnect
+    {
+        public override void Open()
+        {
+            Console.WriteLine("SQL Connection Opened: ");
+        }
+
+        public override void Closed()
+        {
+            Console.WriteLine("SQL Connection Closed: ");
+        }
+    }
+
+    public class OracleConnection : DBConnect
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Oracle Connection Opened: ");
+        }
+
+        public override void Closed()
+        {
+            Console.WriteLine("Oracle Connection Closed: ");
+        }
+    }
+
+    public class DbCommand
+    {
+        public DbCommand(string connection)
+        {
+            Console.WriteLine("DbCommand initialized: ");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
